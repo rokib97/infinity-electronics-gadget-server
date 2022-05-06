7aa65b3c4d981b5001e9b476f931a0992b7dd10c
rokib97 <hasanrokib1997@gmail.com>
2022-05-06 23:39:03 +0600

    connected to database and implemented get api

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -8,6 +10,37 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wmqrb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client
+      .db("infinityElectronics")
+      .collection("products");
+
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+// client.connect((err) => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   console.log("db connected");
+//   client.close();
+// });
 
 // root api
 app.get("/", (req, res) => {
