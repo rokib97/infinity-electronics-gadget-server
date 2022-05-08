58bdb3e8582d376d924cbaac16b48615629e18bc
rokib97 <hasanrokib1997@gmail.com>
2022-05-08 20:31:12 +0600

    added jwt reveal config vars

--- index.js
+++ index.js
@@ -93,10 +93,9 @@ async function run() {
 
     // get single user product api
     app.get("/items", async (req, res) => {
-      // const authHeader = req.headers.authorization;
-      // console.log(authHeader);
+      const authHeader = req.headers.authorization;
+      console.log(authHeader);
       const email = req.query.email;
-      console.log(req.query.email);
       const query = { email };
       const cursor = productCollection.find(query);
       const products = await cursor.toArray();
