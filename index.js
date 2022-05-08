5cfa2c19ebc3a276ed4dfd2d43c9a9c745b7ef9b
rokib97 <hasanrokib1997@gmail.com>
2022-05-08 22:25:15 +0600

    implemented jwt verify

--- index.js
+++ index.js
@@ -23,9 +23,8 @@ function verifyJWT(req, res, next) {
       return res.status(403).send({ message: "forbidden access" });
     }
     req.decoded = decoded;
+    next();
   });
-
-  next();
 }
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wmqrb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
