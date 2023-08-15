d319f43195642e7470e43941aca653cdb31941c7
rokib97 <hasanrokib1997@gmail.com>
2023-08-15 20:25:20 +0600

    chnaged mongodb driver

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 require("dotenv").config();
@@ -38,6 +37,17 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
+// const uri =
+//   "mongodb+srv://infinityUser:9nuS8e8syJAPYHzI@cluster0.wmqrb.mongodb.net/?retryWrites=true&w=majority";
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
 
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wmqrb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 // const client = new MongoClient(uri, {
