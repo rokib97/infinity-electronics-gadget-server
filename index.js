452b99134ab9697a5038277e6b804e38102adb73
rokib97 <hasanrokib1997@gmail.com>
2023-08-15 20:32:15 +0600

    chnaged mongodb driver

--- index.js
+++ index.js
@@ -57,6 +57,12 @@ const client = new MongoClient(uri, {
 // });
 async function run() {
   try {
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
     const productCollection = client
       .db("infinityElectronics")
       .collection("products");
